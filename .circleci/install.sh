ae38c6391aa13c7eee27ba45746392a27df7d764
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-05-23 01:53:50 -0400

    add setup_remote_docker to CI

--- .circleci/install.sh
+++ .circleci/install.sh
@@ -2,24 +2,9 @@
 
 set -e
 
-
-# setup for Docker
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-
-
 # install OS dependencies
 apt-get update
-
-apt-get install -y --no-install-recommends \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  docker-ce \
-  gnupg2 \
-  locales \
-  software-properties-common
+apt-get install -y locales
 
 
 # fix Meteor/Mongo locale issue on Debian
